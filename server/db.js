ce1662ec04d604a188596b34f94e5210a4fe6aa0
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-03-29 12:21:55 +0530

    env config workgin

--- server/db.js
+++ server/db.js
@@ -1,1 +1,12 @@
 // configuration file
+import pkg from 'pg';
+const { Pool } = pkg;
+const pool = new Pool({
+    user: process.env.PSQL_USER,
+    password: process.env.PSQL_PASS,
+    host: process.env.PSQL_HOST,
+    port: process.env.PSQL_PORT,
+    database: process.env.PSQL_DB
+});
+
+export default pool;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 import epxress from 'express'
 import cors from 'cors'
+import pool from './db.js'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 const app = epxress();
 const PORT = process.env.PORT || 8080;
@@ -9,6 +14,18 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(epxress.json());
 
+// Routes //
+
+// create todo
+
+// get all todo
+
+// get a todo
+
+
+// update all todo
+
+// delete todo
 
 app.listen(PORT, () => {
     console.log("Server running on PORT=" + PORT);
