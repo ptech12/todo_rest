830a61f6b2d119599e7c07b5629288d7958802e9
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-03-29 12:42:51 +0530

    Create and Read api done

--- server/db.js
+++ server/db.js
@@ -1,6 +1,10 @@
 // configuration file
 import pkg from 'pg';
 const { Pool } = pkg;
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const pool = new Pool({
     user: process.env.PSQL_USER,
     password: process.env.PSQL_PASS,
@@ -10,3 +14,9 @@ const pool = new Pool({
 });
 
 export default pool;
+
+/*     user: process.env.PSQL_USER,
+    password: process.env.PSQL_PASS,
+    host: process.env.PSQL_HOST,
+    port: process.env.PSQL_PORT,
+    database: process.env.PSQL_DB */
--- server/index.js
+++ server/index.js
@@ -1,32 +1,65 @@
-import epxress from 'express'
-import cors from 'cors'
-import pool from './db.js'
-import dotenv from 'dotenv';
+import epxress from "express";
+import cors from "cors";
+import pool from "./db.js";
+import dotenv from "dotenv";
 
 dotenv.config();
-
 
 const app = epxress();
 const PORT = process.env.PORT || 8080;
 
-
 // middleware
 app.use(cors());
-app.use(epxress.json());
+app.use(epxress.json()); // req.body
 
 // Routes //
 
 // create todo
 
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
 // get all todo
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+
+    res.status(200).json(allTodos.rows);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // get a todo
 
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1 ", [
+      id,
+    ]);
+    res.json(todo.rows)
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 // update all todo
 
 // delete todo
 
 app.listen(PORT, () => {
-    console.log("Server running on PORT=" + PORT);
-})
+  console.log("Server running on PORT=" + PORT);
+});
