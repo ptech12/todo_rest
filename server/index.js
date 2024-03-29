84d6f1b72c6d9ac3e6876a28268517fcf80c3fff
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-03-29 12:47:03 +0530

    Update todos done

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.post("/todos", async (req, res) => {
 
     res.json(newTodo.rows[0]);
   } catch (err) {
-    console.error(err);
+    console.error(err.message);
   }
 });
 
@@ -37,8 +37,8 @@ app.get("/todos", async (req, res) => {
     const allTodos = await pool.query("SELECT * FROM todo");
 
     res.status(200).json(allTodos.rows);
-  } catch (error) {
-    console.error(error);
+  } catch (err) {
+    console.error(err.message);
   }
 });
 
@@ -50,13 +50,27 @@ app.get("/todos/:id", async (req, res) => {
     const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1 ", [
       id,
     ]);
-    res.json(todo.rows)
-  } catch (error) {
-    console.log(error);
+    res.json(todo.rows);
+  } catch (err) {
+    console.error(err.message);
   }
 });
 
 // update all todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id }= req.params;
+    const { description } = req.body;
+    // update a todo
+    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",
+        [ description, id ]  
+    )
+
+    res.json("Todo was updated")
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // delete todo
 
