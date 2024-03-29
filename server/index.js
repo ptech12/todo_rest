ba29e856554067c59212bee6620b21d140ed06e5
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-03-29 12:51:10 +0530

    DELETE request done

--- server/index.js
+++ server/index.js
@@ -59,20 +59,33 @@ app.get("/todos/:id", async (req, res) => {
 // update all todo
 app.put("/todos/:id", async (req, res) => {
   try {
-    const { id }= req.params;
+    const { id } = req.params;
     const { description } = req.body;
     // update a todo
-    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",
-        [ description, id ]  
-    )
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
 
-    res.json("Todo was updated")
+    res.json("Todo was updated");
   } catch (err) {
     console.error(err.message);
   }
 });
 
 // delete todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+
+    res.json(`todo_id=${id} deleted succeesfully`)
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(PORT, () => {
   console.log("Server running on PORT=" + PORT);
