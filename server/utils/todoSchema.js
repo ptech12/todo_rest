a25a2b6289b888f7f7f93a2fe7fd0b9bdd9cac2c
Prasanth A R <73158535+ptech12@users.noreply.github.com>
2024-03-30 13:04:06 +0530

    add code comments

--- server/utils/todoSchema.js
+++ server/utils/todoSchema.js
@@ -1,8 +1,14 @@
 const Joi = require('joi');
 
-
 const todoSchema = Joi.object({
+    // Setting validation rules for the 'description' field: it must be a string, have a minimum length of 3 characters, 
+    // and be required
     description: Joi.string().min(3).required()
 })
 
+/**
+ * 
+ * @param {object} todo - body of the request
+ * @returns error if validation not accepted
+ */
 exports.validateTask = (todo) => todoSchema.validate(todo)
